1271dd2059a93e1a3e3ed04b72918d5ad8c65f0d
mohamed <hwerthimohamed25@gmail.com>
2024-04-06 23:59:17 +0200

    seconnd commmit front

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SignInRequestModel } from 'src/app/models/Request/RequestModel';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
-  navigateTo() {
-    this.router.navigate(['/register']);
-  }
-  navigateToApp() {
-    console.log('navigate to App');
+  constructor(private router: Router, private authService: AuthService) {}
+  payload: SignInRequestModel = {
+    email: '',
+    password: '',
+  };
 
-    this.router.navigate(['home']);
+  submit() {
+    this.SignInRequest();
+  }
+
+  SignInRequest() {
+    this.authService.singInAdmin(this.payload).subscribe(
+      (res) => {
+        console.log('res ');
+        console.log(res);
+        localStorage.setItem('role', JSON.stringify(res.role));
+        alert('login Sucess');
+        this.router.navigate(['home']);
+      },
+      (err: HttpErrorResponse) => {
+        alert(err.error.message);
+      }
+    );
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AdminSignUpRequestModel } from 'src/app/models/Request/RequestModel';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private router: Router) {}
-  navigateTo() {
+  payload: AdminSignUpRequestModel = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  };
+  constructor(private router: Router, private authService: AuthService) {}
+  submit() {
+    this.signUpUser();
+    alert('Admin registered');
     this.router.navigate(['/login']);
+  }
+
+  signUpUser() {
+    this.authService.singUpAdmin(this.payload).subscribe(
+      (res) => {},
+      (err) => {}
+    );
   }
 }
